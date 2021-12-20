ae56775bc2befe7886f2a9a9cc2477527313bf0d
Jose Benitez <jlbenitez4@utpl.edu.ec>
2021-12-19 23:33:54 -0500

    Cambios Tarea

--- EjerciciosT9/src/paquete1/Ejercicio04.java
+++ EjerciciosT9/src/paquete1/Ejercicio04.java
@@ -16,12 +16,27 @@ public class Ejercicio04 {
      */
     public static void main(String[] args) {
         
+     String acumulador = " ";
         double[] promedios = {10, 10, 9.1, 7, 6.1, 4, 8};
-        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", 
+        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan",
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
         String[] promediosCualitativos = new String[7];
-        
+        for (int i = 0; i < estudiantes.length; i++) {
+            if (promedios[i] >= 0 && promedios[i] <= 5.9) {
+                promediosCualitativos[i] = "Promedio Regular";
+            } else {
+                if (promedios[i] >= 6 && promedios[i] <= 8.9) {
+                    promediosCualitativos[i] = "Promedio Bueno";
+                } else {
+                    promediosCualitativos[i] = "Promedio Sobresaliente";
+                }
+            }
+            acumulador = String.format("%s%s promedio: %.2f %s\n", acumulador
+                    , estudiantes[i], promedios[i]
+                    , promediosCualitativos[i]);
+        }
+        System.out.println(acumulador);
     }
 
 }
